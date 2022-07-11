ef36273bcfea89c5f6c1e934cd399032de467aa7
Guidzs <guidz.2004@gmail.com>
2022-07-11 19:48:35 -0300

    requisito 4

--- script.js
+++ script.js
@@ -12,18 +12,6 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ sku, name, image }) => {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-};
-
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
@@ -36,6 +24,28 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+};
+
+const appendCardItens = async (item) => {
+  const cardItems = document.querySelector('.cart__items');
+  const idItem = getSkuFromProductItem(item);
+  const { id: sku, title: name, price: salePrice } = await fetchItem(idItem);
+  const itemCart = createCartItemElement({ sku, name, salePrice });
+  cardItems.appendChild(itemCart);
+};
+
+const createProductItemElement = ({ sku, name, image }) => {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  button.addEventListener('click', () => appendCardItens(section));
+  section.appendChild(button);
+
+  return section;
 };
 
 const appendItens = async () => {
